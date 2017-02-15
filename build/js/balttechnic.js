6b9a21626a7ddaab2cd3e91c7a472560f144c217
Nadia Degtyareva <hen04@rambler.ru>
2017-02-15 11:27:42 +0300

    some changes

--- build/js/balttechnic.js
+++ build/js/balttechnic.js
@@ -8,18 +8,18 @@ $(function(){
 	$('.right-pic, .right-pic-bg').css('height', height);
 
 	$('.info__img-text').hide();
-	$('.info__item').on('click', function(){
+	$('.info--technics .info__item').on('click', function(){
 		if ( $(this).hasClass('open') ) {
 			$('.info__img-text').slideUp();
-			$('.info__item').removeClass('open');
+			$('.info--technics .info__item').removeClass('open');
 		} else {
 			$('.info__img-text').slideUp();
-			$('.info__item').removeClass('open');
+			$('.info--technics .info__item').removeClass('open');
 			$(this).find('.info__img-text').slideDown();
 			$(this).addClass('open');
-			var img_left = $(this).data('img-left'), 
+			var img_left = $(this).data('img-left'),
 					img_right = $(this).data('img-right');
-			$('.left-pic').css('background-image', 'url('+img_left+')');
+			$('.left-pic-bg').css('background-image', 'url('+img_left+')');
 			$('.right-pic-bg').css('background-image', 'url('+img_right+')');
 		}
 	});
